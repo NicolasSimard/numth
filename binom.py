b5022ff44445e6b2ea2b9e13de6c40953cad08fe
Nicolas Simard <simard.nicolas.0@gmail.com>
2015-05-29 19:51:55 -0400

    Add files binom.py and num_pol_exp.py to numth package.
    The binom module contains a function that computes the binomial coefficients.
    The num_pol_exp.py module contain a function that computes the coefficients of
    a numerical given polynomial in the standard basis of numerical polynomials
    consisting of binomial polynomials.

---
+++ binom.py
@@ -0,0 +1,32 @@
+"""This module contains a function that computes the binomial coefficients.
+
+The function binom(n,k) returns the binomial coefficient n!/(k!(n-k)!). The
+binomial terms are computed recursively.
+
+Examples:
+>>> binom(4,2)
+6
+>>> binom(10,1)
+10
+>>> sum(binom(10,k) for k in range(11)) == 2**10
+True
+"""
+
+def binom(n,k):
+    assert 0 <= k and k <= n
+    binoms = [[1]]
+    for i in range(1,n+1):
+        binoms.append([0]*(i+1))
+    if k == 0 or k == n:
+        binoms[n][k] = 1
+        return 1
+    if not binoms[n][k] == 0:
+        return binoms[n][k]
+    binoms[n][k] = binom(n-1,k-1) + binom(n-1,k)
+    return binoms[n][k]
+
+if __name__ == "__main__":
+    import sys, doctest
+    if len(sys.argv) == 3:
+        print(binom(int(sys.argv[1]),int(sys.argv[2])))
+    doctest.testmod()
---
+++ num_pol_exp.py
@@ -0,0 +1,41 @@
+"""Compute the coefficients of the numerical polynomial in the standard basis.
+
+The polynomials (x|n)=x(x-1)...(x-n+1)/n! form the standard basis for the group
+of numerical polynomials, i.e. the polynomials that take integral values at
+integers. An example of numerical polynomial is x(x+1)/2. This code computes
+the expansion of a numerical polynomial in this basis.
+
+-input: a0 a1 a2 ... an d, where the numerical polynomial is
+(a0 + a1*x + a2*x^2 + ... + an*x^n)/d, with an != 0.
+
+-output: [c0,c1,...,cn], the coefficients of the expansion
+
+Examples:
+>>> num_pol_exp([0, -1, 1, 2]) # The polynomial x(x-1)/2
+[0, 0, 1]
+>>> num_pol_exp([0, 1, 0, -7, 0, 21, 21, 6, 42]) # Sum of 6th powers
+[0, 1, 63, 602, 2100, 3360, 2520, 720]
+"""
+import binom
+
+def num_pol_exp(coeff):
+    f=[0]*len(coeff)
+    d = coeff.pop() # change to coeff[-1]
+    for k in range(len(coeff)):
+        power = 1
+        for c in coeff: # change to coeff[:-1]
+            f[k] += c*power
+            power *= k
+        f[k] /= d
+        f[k] = int(f[k])
+    # computing the coefficients in the basis, up to the precision
+    basis = []
+    for n in range(len(coeff)):
+        basis.append(sum([(-1)**(n-k)*binom.binom(n,k)*f[k] for k in range(n+1)]))
+    return basis
+
+if __name__ == "__main__":
+    import sys, doctest
+    if len(sys.argv) > 1:
+        print(num_pol_exp(list(map(int,sys.argv[1:]))))
+    doctest.testmod()
