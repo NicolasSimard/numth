f8e27e34e11299410b55edec2d2ba69434c3f894
Nicolas Simard <simard.nicolas.0@gmail.com>
2015-06-11 23:07:30 -0400

    Add the cachetools.py module to numth and update .gitignore.
    The .gitignore file is updated to ignore the cache directory func_cache/ which
    is created by the module.

---
+++ cachetools.py
@@ -0,0 +1,52 @@
+import os, json, atexit
+
+class cache_calls:
+    """This decorator caches values of one parameter functions (e.g.
+    arithmetic functions). For example, the function
+
+    @cache_calls
+    def f(n):
+        ...
+        return ...
+
+    will have all its values cached, even between sessions. For example,
+    if f(5) is computed in a python sesion, it will be stored in the file
+    f.cache in the flder func_cache and if f(5) is needed in a future
+    session, it will be read from the cached file (instead of being
+    re-computed).
+    """
+
+    def __init__(self, func):
+        """Initialise the decorator with the cached values."""
+
+        if not os.path.exists('func_cache'):
+            os.makedirs('func_cache')
+        self.cache_file = 'func_cache/' + func.__name__ + '.cache'
+        if os.path.isfile(self.cache_file):
+            with open(self.cache_file,'r') as f:
+                self.cached_calls = json.load(f)
+        else:
+            self.cached_calls = {}
+        self.func = func
+        self.__doc__ = func.__doc__
+        atexit.register(self.close) # Make sure the close function is called
+
+    def __call__(self,n):
+        # When json serializes a dictionnary, it stores the keys as strings,
+        # since an integer cannot be the key of a dictionnary in Javascript.
+        # We therefore convert the key to a string.
+        if str(n) in self.cached_calls:
+            return self.cached_calls[str(n)]
+        else:
+            self.cached_calls[str(n)] = self.func(n)
+            return self.cached_calls[str(n)]
+
+    def close(self):
+        """Dump the calls in the cache file when session is exited.
+
+        When the python session is exited, the cached calls (stored in the
+        dictionnary self.cached_calls) are stored in the cache file for later
+        use.
+        """
+        with open(self.cache_file,'w') as f:
+            json.dump(self.cached_calls,f)
