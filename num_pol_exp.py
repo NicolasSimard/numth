35aa361bb49502fc28eeaf5cb5b9a958890fdf15
Nicolas Simard <simard.nicolas.0@gmail.com>
2017-04-13 10:14:56 -0400

    Small change in the documentation.

--- num_pol_exp.py
+++ num_pol_exp.py
@@ -1,24 +1,24 @@
-"""Compute the coefficients of the numerical polynomial in the standard basis.
-
-The polynomials (x|n)=x(x-1)...(x-n+1)/n! form the standard basis for the
-group of numerical polynomials, i.e. the polynomials that take integral values
-at integers. An example of numerical polynomial is x(x+1)/2. This code
-computes the expansion of a numerical polynomial in this basis.
-
--input: a0 a1 a2 ... an d, where the numerical polynomial is
-(a0 + a1*x + a2*x^2 + ... + an*x^n)/d, with an != 0.
-
--output: [c0,c1,...,cn], the coefficients of the expansion
-
-Examples:
->>> num_pol_exp([0, -1, 1, 2]) # The polynomial x(x-1)/2
-[0, 0, 1]
->>> num_pol_exp([0, 1, 0, -7, 0, 21, 21, 6, 42]) # Sum of 6th powers
-[0, 1, 63, 602, 2100, 3360, 2520, 720]
-"""
 import binom
 
 def num_pol_exp(coeff):
+	"""Compute the coefficients of the numerical polynomial in the standard basis.
+
+	The polynomials (x|n)=x(x-1)...(x-n+1)/n! form the standard basis for the
+	group of numerical polynomials, i.e. the polynomials that take integral values
+	at integers. An example of numerical polynomial is x(x+1)/2. This code
+	computes the expansion of a numerical polynomial in this basis.
+
+	-input: a0 a1 a2 ... an d, where the numerical polynomial is
+	(a0 + a1*x + a2*x^2 + ... + an*x^n)/d, with an != 0.
+
+	-output: [c0,c1,...,cn], the coefficients of the expansion
+
+	Examples:
+	>>> num_pol_exp([0, -1, 1, 2]) # The polynomial x(x-1)/2
+	[0, 0, 1]
+	>>> num_pol_exp([0, 1, 0, -7, 0, 21, 21, 6, 42]) # Sum of 6th powers
+	[0, 1, 63, 602, 2100, 3360, 2520, 720]
+	"""
     f=[0]*len(coeff)
     d = coeff.pop() # change to coeff[-1]
     for k in range(len(coeff)):
