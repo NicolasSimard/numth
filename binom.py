97a2d7b4b24e534afe188c0ba829755402df6cc0
Nicolas Simard <simard.nicolas.0@gmail.com>
2017-04-13 09:38:01 -0400

    Move docstrings inside binom function.

--- binom.py
+++ binom.py
@@ -1,18 +1,17 @@
-"""This module contains a function that computes the binomial coefficients.
-
-The function binom(n,k) returns the binomial coefficient n!/(k!(n-k)!). The
-binomial terms are computed recursively.
-
-Examples:
->>> binom(4,2)
-6
->>> binom(10,1)
-10
->>> sum(binom(10,k) for k in range(11)) == 2**10
-True
-"""
-
 def binom(n,k):
+	"""This module contains a function that computes the binomial coefficients.
+
+	The function binom(n,k) returns the binomial coefficient n!/(k!(n-k)!). The
+	binomial terms are computed recursively.
+
+	Examples:
+	>>> binom(4,2)
+	6
+	>>> binom(10,1)
+	10
+	>>> sum(binom(10,k) for k in range(11)) == 2**10
+	True
+	"""
     assert 0 <= k and k <= n
     binoms = [[1]]
     for i in range(1,n+1):
